761010430accc24a6ef4b7373d59ca6846e69131
Josh Fox <jjv360@me.com>
2017-08-18 20:24:46 +0200

    Added ultrasonic sensor driver (incomplete)

--- init.js
+++ init.js
@@ -6,6 +6,8 @@ console.log("Loading drivers...")
 // Drivers
 const UltrasonicDistanceSensor = require("./src/drivers/pi/sensors/UltrasonicDistanceSensor")
 
+// Setup bot
+const Bot = require("./src/index")
 var bot = new Bot();
 bot.registerDevice(new UltrasonicDistanceSensor(0, -1, Math.PI, 180, { triggerPin: 16, echoPin: 18 }))
 
