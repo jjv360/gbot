f30c7c3a30fd5d56de861200216a0a10fada6a85
Josh Fox <jjv360@me.com>
2017-08-19 22:06:56 +0200

    LCD

--- src/drivers/pi/actuators/LCD.js
+++ src/drivers/pi/actuators/LCD.js
@@ -12,12 +12,26 @@ module.exports = class LCD extends Device {
         // Store values
         this.serial = config.serial
 		this.type = Device.Type.Log
+        this.buffer = "..."
+
+        // Write to screen all the time
+        setInterval(this.update.bind(this), 1000)
 
     }
 
     log(txt) {
 
-        Serial.get(this.serial).writeln("LCD " + txt);
+        // Update buffer
+        this.buffer = txt
+        this.update()
+
+    }
+
+    /** Update the screen */
+    update() {
+
+        // Write to serial
+        Serial.get(this.serial).writeln("LCD " + this.buffer);
 
     }
 
