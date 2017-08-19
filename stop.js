acabe4112391f78d1ef3a2bd50c4f0a1847a6101
Josh Fox <jjv360@me.com>
2017-08-20 00:50:51 +0200

    LCD

--- stop.js
+++ stop.js
@@ -2,4 +2,5 @@
 const Serial = require("./src/drivers/pi/gateway/Serial")
 
 // Write to serial
-Serial.get("/dev/ttyUSB0").writeln(this.buffer);
+Serial.get("/dev/ttyUSB0").writeln("WHEEL1 0");
+Serial.get("/dev/ttyUSB0").writeln("WHEEL2 0");
